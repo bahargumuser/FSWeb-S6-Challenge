68fe1e831ca9c069d68beda0b7181ea367f1b7e6
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-04-20 12:20:28 +0300

    dün akşam yapılanlar

--- src/App.js
+++ src/App.js
@@ -1,65 +1,37 @@
-import React from "react";
+import "./App.css";
+import Main from "./Main";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import styled from "styled-components";
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const styleAppCo = styled.div`
+  width: 50%;
+  margin: 0 auto;
+`;
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  const [starWchars, setstarWchars] = useState();
-  let showList = false;
-
-  if (starWchars) {
-    showList = true;
-  } else {
-    showList = false;
-  }
-
-  function GetAPIData(sParam) {
-    useEffect(() => {
-      axios
-        .get("https://swapi.dev/api/people", {
-          params: {
-            search: sParam,
-          },
-        })
-        .then((res) => {
-          setstarWchars(res.data.results);
-          console.log(res.data);
-        })
-        .catch((error) => console.log(error));
-    }, []);
-    function makeaSearch(e) {
-      //event olduğu için e yazabildik
-      //setParam(e.target.value);
-      console.log(e.target.value);
-      GetAPIData(e.target.value);
-    }
-    useEffect(() => {
-      GetAPIData();
-    }, []);
-
-    return (
-      <div className="App">
-        <h1 className="Header">Karakterler</h1>
-        <form>
-          <input type="text" placeHolder="Search" onChange={makeaSearch} />
-        </form>
-        return ({!starWchars && <div> Arama yapınız. </div>}
-        {starWchars && (
-          <ul>
-            {starWchars?.map((b) => (
-              <li>{b.name}</li>
-            ))}
-          </ul>
-        )}
-      </div>
-    );
-  }
-};
-
+function App() {
+  const [data, setData] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const handlePage = (value) => {
+    setPage(value);
+  };
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people/?page=${page}`)
+      .then((res) => setData(res.data.results))
+      .catch((err) => console.log(err));
+    axios
+      .get(`https://swapi.dev/api/films/`)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err));
+  }, [page]);
+  return (
+    <styleAppCo>
+      <main>
+        <Main data={data} movies={movies} handlePage={handlePage} page={page} />
+      </main>
+    </styleAppCo>
+  );
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // This is for the fake API. Do not delete!
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import "./App.css";
+import App from "./App.js";
+import reportWebVitals from "./reportWebVitals";
 
-import { worker } from "./mocks/browser";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+/* import { worker } from "./mocks/browser";
 worker.start();
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ */
